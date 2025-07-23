fb91d2b734ba8d4c27f23068a40140ace2ab965d
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2025-07-22 18:02:12 -0700

    uploaded satoshi font and chagned in tailwindconfig.mjs and globals.css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -95,7 +95,7 @@ const config = {
       },
       fontFamily: {
         mono: ['var(--font-geist-mono)'],
-        sans: ['var(--font-geist-sans)'],
+        sans: ['Satoshi', 'sans-serif'],
       },
       keyframes: {
         'accordion-down': {
