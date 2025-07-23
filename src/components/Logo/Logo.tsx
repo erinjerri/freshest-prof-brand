27bc5967ef2f29b4b3fffbf4dfc1aa1e6004d960
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2025-07-22 17:45:49 -0700

    changed link to svg file for main logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -23,7 +23,7 @@ export const Logo = (props: Props) => {
       fetchPriority={priority}
       decoding="async"
       className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="http://raw.githubusercontent.com/erinjerri/freshest-prof-brand/main/public/favicon.ico"
+      src="http://raw.githubusercontent.com/erinjerri/freshest-prof-brand/main/public/favicon.svg"
     />
   )
 }
