0c945066fe1e234bbaa1940f61fb9377cc42dcf8
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2025-08-25 16:20:24 -0700

    clear bulid cache and deploy

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -15,31 +15,15 @@ import { generateMeta } from '@/utilities/generateMeta'
 import PageClient from './page.client'
 import { LivePreviewListener } from '@/components/LivePreviewListener'
 
-export async function generateStaticParams() {
-  const payload = await getPayload({ config: configPromise })
-  const posts = await payload.find({
-    collection: 'posts',
-    draft: false,
-    limit: 1000,
-    overrideAccess: false,
-    pagination: false,
-    select: {
-      slug: true,
-    },
-  })
-
-  const params = posts.docs.map(({ slug }) => {
-    return { slug }
-  })
-
-  return params
-}
-
 type Args = {
   params: Promise<{
     slug?: string
   }>
 }
+
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const dynamicParams = true
 
 export default async function Post({ params: paramsPromise }: Args) {
   const { isEnabled: draft } = await draftMode()
