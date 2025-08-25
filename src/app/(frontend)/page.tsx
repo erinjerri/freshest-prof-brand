b78a6f96348f286d19ce489317ad6b84017a1fb4
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2025-08-25 16:34:48 -0700

    redeploy --> clear build cache and deploy

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,9 @@
 import PageTemplate, { generateMetadata } from './[slug]/page'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const dynamicParams = true
+
 export default PageTemplate
 
 export { generateMetadata }
